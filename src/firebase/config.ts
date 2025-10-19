e6e300f7d88a591085845139fe9e2667c3e381cc
Leonardo Yuuki Nakazone <leoyuuki2005@gmail.com>
2025-10-19 16:47:16 +0000

    Uma pergunta,  os dados da config do firebase nao deveriam estar no .env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANKnkA1zrs4TU3OeFzCLYWk6Lkfk70Gh0",
-  authDomain: "booklove-microsaas.firebaseapp.com",
-  projectId: "booklove-microsaas",
-  storageBucket: "booklove-microsaas.appspot.com",
-  messagingSenderId: "1054144080507",
-  appId: "1:1054144080507:web:cb1eca7ac412d9fde810de"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
