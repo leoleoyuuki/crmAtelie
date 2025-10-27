8b712310c2e4c1ff9d047195dc1031cf2461bce6
Leonardo Yuuki Nakazone <leoyuuki2005@gmail.com>
2025-10-27 16:18:20 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/components/dashboard/order-form-dialog.tsx
+++ src/components/dashboard/order-form-dialog.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import React, { useEffect, useState } from "react";
@@ -125,7 +126,9 @@ export function OrderFormDialog({
       if (order) {
           form.reset({
             ...order,
-            items: order.items.map(item => ({...item, description: item.description ?? '', assignedTo: item.assignedTo ?? ''})),
+            items: order.items 
+                ? order.items.map(item => ({...item, description: item.description ?? '', assignedTo: item.assignedTo ?? ''})) 
+                : defaultValues.items,
           });
       } else {
           form.reset(defaultValues);
